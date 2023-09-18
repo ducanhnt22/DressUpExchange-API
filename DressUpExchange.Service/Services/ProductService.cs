2a133ba503fb77cd899676337cd81d727035db65
ducanhnt22 <ducanhbon4@gmail.com>
2023-09-19 06:55:06 +0700

    user only refresh token, done product crud

--- DressUpExchange.Service/Services/ProductService.cs
+++ DressUpExchange.Service/Services/ProductService.cs
@@ -1,6 +1,16 @@
-﻿using System;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using DressUpExchange.Data.Entity;
+using DressUpExchange.Data.UnitOfWork;
+using DressUpExchange.Service.DTO.Request;
+using DressUpExchange.Service.DTO.Response;
+using DressUpExchange.Service.Exceptions;
+using DressUpExchange.Service.Helpers;
+using DressUpExchange.Service.Ultilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +18,146 @@ namespace DressUpExchange.Service.Services
 {
     public interface IProductService
     {
-
+        Task<PagedResult<ProductResponse>> GetProducts(ProductRequest request, PagingRequest paging);
+        Task<ProductResponse> DeleteProduct(int id);
+        Task<ProductResponse> CreateProduct(ProductRequest request);
+        Task<ProductResponse> GetProductById(int id);
+        Task<ProductResponse> UpdateProduct(int id, ProductRequest request);
     }
-    public class ProductService
+    public class ProductService : IProductService
     {
+        private readonly IProductService _productService;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public ProductService(IProductService productService, IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _productService = productService;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ProductResponse> CreateProduct(ProductRequest product)
+        {
+            try
+            {
+                if (product == null)
+                {
+                    throw new CrudException(HttpStatusCode.BadRequest, "Product Invalid!!!", "");
+                }
+
+                var p = _mapper.Map<Product>(product);
+                _unitOfWork.Repository<Product>().CreateAsync(p);
+                await _unitOfWork.CommitAsync();
+                return _mapper.Map<Product, ProductResponse>(p);
+
+            }
+            catch (CrudException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw new CrudException(HttpStatusCode.BadRequest, "Insert Product Error!!!", ex.InnerException?.Message);
+            }
+        }
+
+        public async Task<ProductResponse> DeleteProduct(int id)
+        {
+            try
+            {
+                var product = await _unitOfWork.Repository<Product>().GetAsync(p => p.ProductId == id);
+
+                if (product == null)
+                {
+                    throw new CrudException(HttpStatusCode.NotFound, "Not found product with id", id.ToString());
+                }
+
+                _unitOfWork.Repository<Product>().Delete(product);
+                await _unitOfWork.CommitAsync();
+                return _mapper.Map<Product, ProductResponse>(product);
+            }
+            catch (CrudException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw new CrudException(HttpStatusCode.BadRequest, "Delete Product Error!!!", ex.InnerException?.Message);
+            }
+        }
+
+        public async Task<ProductResponse> GetProductById(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    throw new CrudException(HttpStatusCode.BadRequest, "Id Product Invalid", "");
+                }
+                var response = await _unitOfWork.Repository<Product>().GetAsync(p => p.ProductId == id);
+
+                if (response == null)
+                {
+                    throw new CrudException(HttpStatusCode.NotFound, "Not found product with id", "");
+                }
+
+                return _mapper.Map<ProductResponse>(response);
+            }
+            catch (CrudException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw new CrudException(HttpStatusCode.BadRequest, "Get Product By ID Error!!!", ex.InnerException?.Message);
+            }
+        }
+
+        public Task<PagedResult<ProductResponse>> GetProducts(ProductRequest request, PagingRequest paging)
+        {
+            try
+            {
+                var filter = _mapper.Map<ProductResponse>(request);
+                var products = _unitOfWork.Repository<Product>().GetAll()
+                                           .ProjectTo<ProductResponse>(_mapper.ConfigurationProvider)
+                                           .DynamicFilter(filter)
+                                           .ToList();
+                var sort = PageHelper<ProductResponse>.Sorting(paging.SortType, products, paging.ColName);
+                var result = PageHelper<ProductResponse>.Paging(sort, paging.Page, paging.PageSize);
+                return Task.FromResult(result);
+            }
+            catch (CrudException ex)
+            {
+                throw new CrudException(HttpStatusCode.BadRequest, "Get product list error!!!!!", ex.Message);
+            }
+        }
+
+        public async Task<ProductResponse> UpdateProduct(int id, ProductRequest request)
+        {
+            try
+            {
+                Product product = _unitOfWork.Repository<Product>()
+                     .Find(p => p.ProductId == id);
+
+                if (product == null)
+                {
+                    throw new CrudException(HttpStatusCode.NotFound, "Not found product with id", id.ToString());
+                }
+
+                _mapper.Map<ProductRequest, Product>(request, product);
+
+                await _unitOfWork.Repository<Product>().Update(product, id);
+                await _unitOfWork.CommitAsync();
+                return _mapper.Map<Product, ProductResponse>(product);
+            }
+            catch (CrudException ex)
+            {
+                throw new CrudException(HttpStatusCode.BadRequest, "Update product error!!!!!", ex.Message);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
---
+++ DressUpExchange/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using DressUpExchange.Service.DTO.Request;
+using DressUpExchange.Service.DTO.Response;
+using DressUpExchange.Service.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DressUpExchange.API.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<ProductResponse>>> GetProducts([FromQuery] PagingRequest pagingRequest, [FromQuery] ProductRequest productRequest)
+        {
+            var rs = await _productService.GetProducts(productRequest, pagingRequest);
+            return Ok(rs);
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProductResponse>> GetProduct(int id)
+        {
+            var rs = await _productService.GetProductById(id);
+            return Ok(rs);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<ProductResponse>> UpdateProduct([FromBody] ProductRequest productRequest, int id)
+        {
+            var rs = await _productService.UpdateProduct(id, productRequest);
+            if (rs == null) return NotFound();
+            return Ok(rs);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<ProductResponse>> DeleteProduct(int id)
+        {
+            var rs = await _productService.DeleteProduct(id);
+            if (rs == null) return NotFound();
+            return Ok(rs);
+        }
+        [HttpPost()]
+        public async Task<ActionResult<UserResponse>> CreateProduct([FromBody] ProductRequest model)
+        {
+            var rs = await _productService.CreateProduct(model);
+            return Ok(rs);
+        }
+    }
+}
