e2ca51788fadd0068915f6391488f9f53c847628
ducanhnt22 <ducanhbon4@gmail.com>
2023-09-26 05:10:05 +0700

    Add JWT, add Authorize to API, but just user

--- DressUpExchange/Controllers/UsersController.cs
+++ DressUpExchange/Controllers/UsersController.cs
@@ -3,8 +3,10 @@ using DressUpExchange.Service.DTO.Response;
 using DressUpExchange.Service.Exceptions;
 using DressUpExchange.Service.Services;
 using Google.Apis.Auth.OAuth2.Requests;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NTQ.Sdk.Core.Filters;
+using System.Data;
 using System.Net;
 
 namespace DressUpExchange.API.Controllers
@@ -20,12 +22,14 @@ namespace DressUpExchange.API.Controllers
         }
 
         [HttpGet]
+        [Authorize(Roles = "Admin")]
         public async Task<ActionResult<List<UserResponse>>> GetCustomer([FromQuery] PagingRequest pagingRequest, [FromQuery] CustomerRequest customerRequest)
         {
             var result = await _customerService.GetCustomers(customerRequest, pagingRequest);
             return Ok(result);
         }
         [HttpGet("{id:int}")]
+        [Authorize(Roles = "Admin")]
         public async Task<ActionResult<UserResponse>> GetCustomer(int id)
         {
             var result = await _customerService.GetCustomerById(id);
@@ -44,12 +48,14 @@ namespace DressUpExchange.API.Controllers
             return Ok(rs);
         }
         [HttpPut("{id:int}")]
+        [Authorize(Roles = "Admin")]
         public async Task<ActionResult<UserResponse>> EditProfile(int id, [FromBody] UserRequest request)
         {
             var rs = await _customerService.UpdateAsync(id, request);
             return Ok(rs);
         }
         [HttpPost("RefreshToken")]
+        [Authorize(Roles = "Admin")]
         public async Task<ActionResult<RefreshTokenResponse>> RefreshToken(string refreshToken)
         {
             try
--- DressUpExchange/Program.cs
+++ DressUpExchange/Program.cs
@@ -1,9 +1,13 @@
 using DressUpExchange.API.Mapper;
+using DressUpExchange.API;
 using DressUpExchange.Data.Entity;
 using DressUpExchange.Data.UnitOfWork;
 using DressUpExchange.Service.Services;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,28 +18,98 @@ builder.Services.AddScoped<IFileStorageService, FirebaseStorageService>();
 builder.Services.AddScoped<IFeedbackService, FeedbackService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IVourcherService,VoucherService>();
+builder.Services.AddScoped<IVourcherService, VoucherService>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddAutoMapper(typeof(Mapping));
 builder.Services.AddScoped<DressUpExchanceContext>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add authentication and configure JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JWTSecretKey:Issuer"],
+            ValidAudience = builder.Configuration["JWTSecretKey:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSecretKey:SecretKey"])),
+            ClockSkew = TimeSpan.FromSeconds(1)
+        };
+    });
+
+// Add HttpContextAccessor
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "DressUpExchange-API", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer",
+    });
+    opt.OperationFilter<AuthorizeOperationFilter>();
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.DefaultModelExpandDepth(0);
+    c.DefaultModelsExpandDepth(-1);
+    c.DefaultModelRendering(ModelRendering.Example);
+    c.DisplayOperationId();
+    c.DisplayRequestDuration();
+    c.DocExpansion(DocExpansion.None);
+    c.EnableDeepLinking();
+    c.EnableFilter();
+    c.ShowExtensions();
+    c.EnableValidator();
+});
+
+app.Use((context, next) =>
+{
+    context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+    context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    return next.Invoke();
+});
+app.UseCors();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
