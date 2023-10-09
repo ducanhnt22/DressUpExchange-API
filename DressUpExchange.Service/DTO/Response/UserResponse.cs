3f617e613dfb578a07181bd3e25a6baf9c543c4e
Nắng <99975060+Nang13@users.noreply.github.com>
2023-10-09 15:09:09 +0700

    [Troll]

--- DressUpExchange.Service/DTO/Response/UserResponse.cs
+++ DressUpExchange.Service/DTO/Response/UserResponse.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DressUpExchange.Service.DTO.Response
 {
     public class UserResponse
     {
-<<<<<<< HEAD
-        public int UserId { get; internal set; }
-=======
+        [JsonIgnore]
         public int Id { get; set; }
->>>>>>> 823132cb1cbee19a4be1ebd6b55810d225b64e5d
         public string? Phone { get; internal set; }
         public string? Name { get; internal set; }
         public string? Role { get; internal set; }
